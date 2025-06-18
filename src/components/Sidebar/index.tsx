8339ec1186f88b509037655e9908478bdf59d10c
andolino baisac <andolino.baisac@fligno.com>
2025-06-18 13:50:31 +0800

    20250607

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -810,6 +810,17 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                               For New Loans
                             </Link>
                           </li>
+                          <li>
+                            <Link
+                              href="/for-new-loans"
+                              className={`group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ${
+                                pathname === "/for-new-loans" &&
+                                "text-white"
+                              }`}
+                            >
+                              Audit Trail
+                            </Link>
+                          </li>
                           
                         </ul>
                       </div>
