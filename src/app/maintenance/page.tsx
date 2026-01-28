176962a78ca86141361a8d539913da08174928fa
Thomasraf150 <thomasraf150@gmail.com>
2026-01-27 20:41:56 -1000

    Removing the maintenance button

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -1,31 +1,10 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { AlertTriangle } from "react-feather";
-import DefaultLayout from '@/components/Layouts/DefaultLayout';
-import Breadcrumb from '@/components/Breadcrumbs/Breadcrumb';
 import './styles.css';
-import Link from "next/link";
-import useMaintenanceSwitcher from '@/hooks/useMaintenanceSwitcher';
 
 const Maintenance: React.FC = () => {
-  const { handleSetMaintenanceMode } = useMaintenanceSwitcher();
-  const [userData, setUserData] = useState<any>(null);
-  
-  const setSetMaintenanceMode = (status: boolean) => {
-    handleSetMaintenanceMode(status);
-  }
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const storedAuthStore = localStorage.getItem('authStore') ?? '{}';
-      setUserData(JSON.parse(storedAuthStore)['state']);
-    }
-  }, []);
-
-  useEffect(() => {
-    console.log(userData?.user?.email, 'userData');
-  }, [userData]);
 
   return (
       <div>
@@ -39,14 +18,6 @@ const Maintenance: React.FC = () => {
             Our system is currently under maintenance. We{`'`}re working hard to bring it back online as soon as possible.
             Please check back later.
           </p>
-          {userData?.user?.email === 'donsuperadmin@gmail.com' && (
-            <button 
-              type="button" 
-              className="text-white bg-gradient-to-r items-center from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 flex space-x-2 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-              onClick={ () => setSetMaintenanceMode(false) }>
-                <span>Switch Maintenance Off</span>
-            </button>
-          )}
         </div>
       </main>
       </div>
